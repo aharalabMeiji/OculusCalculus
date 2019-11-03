763a1020ae817e722fdfedeb149eb2010dc74565
Kazushi Ahara <ahara@meiji.ac.jp>
2019-11-03 12:15:09 +0900

    LimitSample1

--- OculusCalculus/Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
+++ OculusCalculus/Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
@@ -385,6 +385,7 @@ public class OVRPlayerController : MonoBehaviour
 #endif
 
 			Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+            if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger)) primaryAxis = Vector2.zero;
 
 			// If speed quantization is enabled, adjust the input to the number of fixed speed steps.
 			if (FixedSpeedSteps > 0)
---
+++ OculusCalculus/Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    public GameObject XAxis, YAxis, ZAxis, XYPlane;
+    public float CoordMax = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setSizeOfSpace(100f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void setSizeOfSpace(float s)
+    {
+        XAxis.transform.localScale = (s + 0.1f) * 2f * Vector3.right + 0.1f * Vector3.up + 0.1f * Vector3.forward;
+        YAxis.transform.localScale = 0.1f * Vector3.right + 0.1f * Vector3.up + (s + 0.1f) * 2f * Vector3.forward;
+        ZAxis.transform.localScale = 0.1f * Vector3.right + (s + 0.1f) * 2f * Vector3.up + 0.1f * Vector3.forward;
+        XYPlane.transform.localScale = s * 2f * Vector3.right + 0.05f * Vector3.up + s * 2f * Vector3.forward;
+    }
+
+}
