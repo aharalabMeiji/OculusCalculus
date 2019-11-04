186582416125e388ca5c6b1e00bff5481f8198d1
Kazushi Ahara <ahara@meiji.ac.jp>
2019-11-04 12:34:57 +0900

    廊下を作った

---
+++ OculusCalculus/Assets/MuseumPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuseumPlayer : MonoBehaviour
+{
+    public static Vector3 PlayerPosition;
+    public static int ContentNumber;
+
+    public GameObject RayCone;
+    public GameObject RayCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = PlayerPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
